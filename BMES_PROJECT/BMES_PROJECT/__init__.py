3ed9a98779db49b8b0de105a4841e2098573cde1
Mobassar <kmobassar@gmail.com>
2020-05-24 05:59:49 +0530

    Add project files.

---
+++ BMES_PROJECT/BMES_PROJECT/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for BMES_PROJECT.
+"""
---
+++ BMES_PROJECT/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mobassar khan\source\repos\BMES\BMES_PROJECT\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
